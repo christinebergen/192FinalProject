30dfe822f4623bd286a1ce215e7e4a2770f7640f
Christine Bergen <christine.bergen@itas.ca>
2023-04-05 13:20:15 -0700

    first commit

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
-# Getting Started with Create React App
+# ITAS 192 Final Project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Traditional Irish Music Randomizer with Wikipedia search
+## By: Christine Bergen
 
-## Available Scripts
 
-In the project directory, you can run:
+### Purpose of this project
+Being a musician with a keen interest in Traditional Irish music, I wanted to provide users a way to randomly choose 
+a Traditional Irish tune from the top 50 most popular tunes on a commonly used site for Irish Music: thesession.org. 
 
-### `npm start`
+With thousands of tunes out there, it can be very challenging to decide which tunes to play next, 
+and this website can help to direct the user towards a popular tune that would be worthwhile to learn.
+This website will also provide some background information about the tune that is displayed via the Wikipedia 
+API. 
+
+### API's used:
+https://thesession.org/api
+https://api.wikimedia.org/core/v1/wikipedia/en/search/page?q=earth&limit=5
+
+## JS code stack
+React JS Library: 
+Anime JS Library: https://animejs.com
+
+
+### Challenges & If I Had More Time...
+
+Initially, after the random tune name was obtained, I wanted to be able to show a page from 
+thesession.org website that showed the sheet music. After some deliberation it was decided that it may be 
+fairly complicated, and that the current timeline for this project likely would not see this feature to completion. 
+I also attempted to add a YouTube search to the site, that searched for the top video using the name of the tune 
+obtained randomly. This proved problematic to access the information needed, so I decided to try the Wikipedia API, which
+seemed to be implemented fairly seamlessly. If I had more time, I would have liked to work a bit more on these 2 pieces of 
+functionality.
+
+### Resources
+I did have some help from Chat GPT for troubleshooting some of the functions.
+
+
+### General Course Feedback
+Overall this was a decent course. I definitely do not feel like I'm confident in any aspect of JS though. It's a pretty tough concept to grasp with no coding experience prior to last September. I would like to see more coding examples given in class that we all go through together. Being told to "refer to the slides" isn't very helpful, as the slides don't lay out the coding in the proper context. "Refer to the slides" format works well for HTML and CSS as they are extremely simple in the land of coding, but I think JS deserves a much deeper dive.
+
+
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import anime from 'animejs/lib/anime.es.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      tunes: [],
+      currentTune: null,
+      wikiResults: [],
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.fetchWikiResults = this.fetchWikiResults.bind(this);
+  }
+
+  async componentDidMount() {
+    const url = 'https://thesession.org/tunes/popular?format=json&perpage=50';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState(
+      {
+        tunes: data.tunes,
+        currentTune: data.tunes[0],
+        loading: false,
+      },
+      () => {
+        this.fetchWikiResults(data.tunes[0].name, data.tunes[0].type);
+      }
+    );
+  }
+
+  async fetchWikiResults(name, type) {
+    const query = `${name} ${type}`;
+    const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&format=json&origin=*`;
+    console.log('fetching wiki results for', name, type);
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.query.search.map((result) => ({
+      ...result,
+      snippet: result.snippet
+        .replace(/<[^>]*>?/gm, '')
+        .replace(/&quot;/gi, '"'),
+    }));
+    this.setState({ wikiResults: results.slice(0, 5) });
+    console.log('wiki results', results);
+  }
+
+  handleButtonClick = () => {
+    try {
+      const { tunes } = this.state;
+      if (tunes.length === 0) {
+        throw new Error('No tunes to choose from');
+      }
+      const randomIndex = Math.floor(Math.random() * tunes.length);
+      const randomTune = tunes[randomIndex];
+      console.log('selected tune', randomTune.name);
+      this.setState({ currentTune: randomTune }, () => {
+        this.fetchWikiResults(randomTune.name, randomTune.type);
+        anime({
+          targets: '.wiki-result-animation',
+          translateY: [100, 0],
+          opacity: [0, 1],
+          easing: 'easeOutQuad',
+          duration: 800,
+          delay: anime.stagger(100),
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // handleButtonClick = () => {
+  //   const { tunes } = this.state;
+  //   const shuffledTunes = [...tunes].sort(() => Math.random() - 0.5);
+  //   const randomIndex = Math.floor(Math.random() * shuffledTunes.length);
+  //   const randomTune = shuffledTunes[randomIndex];
+  //   console.log('selected tune', randomTune.name);
+  //   this.setState({ currentTune: randomTune }, () => {
+  //     this.fetchWikiResults(randomTune.name, randomTune.type);
+
+  //   });
+  // };
+
+  render() {
+    const { loading, currentTune, wikiResults } = this.state;
+
+    return (
+      <div
+        id="root"
+        className="bg-[#98AA9f] flex flex-col justify-center items-center"
+      >
+        <h1 className="text-5xl mt-8 p-4 font-bold w-2/3 text-center bg-[#4D5D53] text-[#CEE1D5] rounded-lg">
+          Traditional Irish Music Randomizer
+        </h1>
+        <h2 className="font-bold p-4">
+          From the top 50 most popular tunes on{' '}
+          <a className="text-blue-900 underline italic" href="thesession.org">
+            The Session
+          </a>
+          , one of the most widely-used sites for traditional irish music
+        </h2>
+        <button
+          className="bg-[#4D5D53] hover:bg-black text-[#CEE1D5] font-bold py-2 px-4 border border-black rounded"
+          onClick={this.handleButtonClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click here to Load Random Tune!
+        </button>
+        {loading || !currentTune ? (
+          <div>loading...</div>
+        ) : (
+          <div className=" flex flex-col lg:flex-row">
+            <div className="bg-[#4D5D53] text-[#CEE1D5] rounded-lg m-8 w-full lg:w-1/2 mr-8">
+              <h2 className="pl-8 pt-4 text-4xl font-bold">
+                {currentTune.name}
+              </h2>
+              <p className="pl-8 text-2xl font-semibold">
+                Tune Type: {currentTune.type}
+              </p>
+              <p className="pl-8 text-2xl font-semibold">
+                Click this link to see the sheet music:{' '}
+              </p>
+              <p className="pl-8 pb-4">
+                <a
+                  className="text-2xl font-semibold underline text-blue-900 italic"
+                  href={currentTune.url}
+                >
+                  {currentTune.name} on The Session
+                </a>
+              </p>
+            </div>
+            <div className=" text-[#CEE1D5] rounded-lg mt-8 mr-8 w-full lg:w-1/2 ml-4">
+              <h3 className="mr-12 ml-12 mb-4 text-2xl text-black font-bold justify-center items-center">
+                Everything you ever wanted to know about {currentTune.name}{' '}
+                {currentTune.type} from Wikipedia:
+              </h3>
+              <ul className="ml-4 mr-12">
+                {wikiResults.map((result) => (
+                  <li
+                    className="wiki-result-animation p-4 bg-[#4D5D53] text-[#CEE1D5] rounded-lg mb-8"
+                    key={result.pageid}
+                  >
+                    <a
+                      className="text-xl font-bold underline"
+                      href={`https://en.wikipedia.org/wiki/irishtune${result.title}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {result.title}
+                    </a>
+                    <p>{result.snippet}...</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
